20f375025bdca55be3d63f50b2dd13a0e69871a3
zabojeb <zabojeb@bk.ru>
2024-09-28 20:48:55 +0300

    Add 3.D and 3.E solutions

--- 3-Binary-Search/D.cpp
+++ 3-Binary-Search/D.cpp
@@ -3,25 +3,25 @@ using namespace std;
 
 int main()
 {
-    int w, h, n;
-    cin >> w >> h >> n;
+    long long n, a, b, w, h;
+    cin >> n >> a >> b >> w >> h;
 
-    long long l = max(w, h), r = 1LL * max(w, h) * n;
+    long long l = 0, r = min(w, h);
 
-    while (l < r)
+    while (l <= r)
     {
-        long long m = (l + r) / 2;
-        if ((m / w) * (m / h) >= n)
+        long long d = (l + r) / 2;
+        if ((w / (a + 2 * d)) * (h / (b + 2 * d)) >= n or (h / (a + 2 * d)) * (w / (b + 2 * d)) >= n)
         {
-            r = m;
+            l = d + 1;
         }
         else
         {
-            l = m + 1;
+            r = d - 1;
         }
     }
 
-    cout << l << endl;
+    cout << r << endl;
 
     return 0;
 }
