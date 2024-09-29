7b4003cae8eb0ae36f2fba9098c5573c9c5f77fd
zabojeb <zabojeb@bk.ru>
2024-09-29 14:07:00 +0300

    Fix 3.I solution

--- 3-Binary-Search/I.cpp
+++ 3-Binary-Search/I.cpp
@@ -3,41 +3,49 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
   int n, k;
+  cin >> n >> k;
 
   vector<int> arr(n);
 
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
     cin >> arr[i];
   }
 
-  int l = 1, r = arr[n-1] + 1;
-  while (l + 1 < r) {
+  int l = 1, r = arr[n - 1] + 1;
+  while (l + 1 < r)
+  {
     int m = (l + r) / 2;
-      
+
     int count = 1;
     int last_pos = arr[0];
-    for (int i = 1; i < n; i++) {
-      if (arr[i] - last_pos >= m) {
+    for (int i = 1; i < n; i++)
+    {
+      if (arr[i] - last_pos >= m)
+      {
         count += 1;
         last_pos = arr[i];
       }
-      if (count >= k) {
+      if (count >= k)
+      {
         break;
       }
     }
 
-  if (count >= k) {
-    l = m;
-  }
-  else {
-    r = m;
-  }
+    if (count >= k)
+    {
+      l = m;
+    }
+    else
+    {
+      r = m;
+    }
   }
 
   cout << l;
 
   return 0;
 }
-
