0677555ff7520ec4e73f8aba76dc8c3564ec7aeb
zabojeb <zabojeb@bk.ru>
2024-09-29 15:56:27 +0300

    Add 3.F solution

---
+++ 3-Binary-Search/F.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    long long a, b, k;
+
+    cin >> a >> b >> k;
+
+    long long l = a, r = b;
+    while (l < r) {
+        long long m = (l + r) / 2;
+
+        long long x = 0;
+    }
+}
