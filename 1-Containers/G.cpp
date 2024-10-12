9f34f5173e1d21f2941d37bbe18d8cc3f94a7fc9
zabojeb <zabojeb@bk.ru>
2024-10-12 15:23:10 +0300

    Add 1.G solution

--- 1-Containers/G.cpp
+++ 1-Containers/G.cpp
@@ -3,62 +3,42 @@
 
 using namespace std;
 
-int main()
-{
-    long long n;
-    cin >> n;
+int main() {
+  int N;
+  cin >> N;
 
-    long long h[100], m[100], evil[100];
-    long long leave_h[100], leave_m[100];
+  int time = 0;
 
-    for (long long i = 0; i < n; ++i)
-    {
-        cin >> h[i] >> m[i] >> evil[i];
+  queue<int> q;
+
+  for (int i = 0; i < N; i++) {
+    int h, m, anger;
+    cin >> h >> m >> anger;
+
+    int arrival_time = h * 60 + m;
+
+    while (!q.empty() and q.front() <= arrival_time) {
+      q.pop();
     }
 
-    queue<pair<long long, long long>> mq;
-    long long curr_h = 0, curr_m = 0;
+    if ((int)q.size() > anger) {
+      int h = arrival_time / 60;
+      int m = arrival_time % 60;
+      cout << h << " " << m << endl;
+    } else {
+      if (time < arrival_time) {
+        time = arrival_time;
+      }
 
-    for (long long i = 0; i < n; ++i)
-    {
-        while (!mq.empty() &&
-               (mq.front().first < h[i] ||
-                (mq.front().first == h[i] && mq.front().second <= m[i])))
-        {
-            mq.pop();
-        }
+      time += 20;
 
-        if ((long long)mq.size() > evil[i])
-        {
-            leave_h[i] = h[i];
-            leave_m[i] = m[i];
-        }
-        else
-        {
-            if (mq.empty())
-            {
-                curr_h = h[i];
-                curr_m = m[i];
-            }
+      int h = time / 60;
+      int m = time % 60;
+      cout << h << " " << m << endl;
 
-            curr_m += 20;
-            if (curr_m >= 60)
-            {
-                curr_m -= 60;
-                curr_h += 1;
-            }
-
-            leave_h[i] = curr_h;
-            leave_m[i] = curr_m;
-
-            mq.push({curr_h, curr_m});
-        }
+      q.push(time);
     }
+  }
 
-    for (long long i = 0; i < n; ++i)
-    {
-        cout << leave_h[i] << " " << leave_m[i] << endl;
-    }
-
-    return 0;
+  return 0;
 }
