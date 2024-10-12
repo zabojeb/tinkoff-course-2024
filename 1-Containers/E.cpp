80fccb9c30765303c8780d31097b83496887e12d
zabojeb <zabojeb@bk.ru>
2024-10-12 12:40:37 +0300

    Add 1.E solution

--- 1-Containers/E.cpp
+++ 1-Containers/E.cpp
@@ -1,110 +1,67 @@
-#include <iostream>
-#include <stack>
-#include <vector>
 #include <algorithm>
-#include <climits>
+#include <deque>
+#include <iostream>
+
+#define push push_back
+#define top back
+#define pop pop_back
 
 using namespace std;
 
-int main()
-{
-    stack<long long> s1_val, s2_val;
-    stack<long long> s1_min, s2_min;
+int main() {
+  int q;
+  cin >> q;
 
-    long long q;
-    cin >> q;
+  deque<long long> s1, s2, min_s1, min_s2;
 
-    for (long long i = 0; i < q; ++i)
-    {
-        long long n;
-        cin >> n;
+  while (q--) {
+    int type;
+    cin >> type;
 
-        if (n == 1)
-        {
-            long long x;
-            cin >> x;
-
-            s1_val.push(x);
-
-            s1_min.push(s1_min.empty() ? x : min(x, s1_min.top()));
+    if (type == 1) {
+      long long x;
+      cin >> x;
+      s1.push(x);
+      if (min_s1.empty()) {
+        min_s1.push(x);
+      } else {
+        min_s1.push(min(min_s1.back(), x));
+      }
+    } else if (type == 2) {
+      if (s2.empty()) {
+        while (!s1.empty()) {
+          s2.push(s1.back());
+          if (min_s2.empty()) {
+            min_s2.push(s1.back());
+          } else {
+            min_s2.push(min(min_s2.back(), s1.back()));
+          }
+          s1.pop();
+          min_s1.pop();
         }
-        else if (n == 2)
-        {
-            if (s2_val.empty())
-            {
-                while (!s1_val.empty())
-                {
-                    long long val = s1_val.top();
-                    s1_val.pop();
-                    s1_min.pop();
+      }
+      cout << s2.back() << endl;
+      s2.pop();
+      min_s2.pop();
+    } else if (type == 3) {
+      int i;
+      cin >> i;
 
-                    s2_val.push(val);
+      i--;
 
-                    long long new_min = s2_min.empty() ? val : min(val, s2_min.top());
-                    s2_min.push(new_min);
-                }
-            }
-
-            long long last_element = s2_val.top();
-            s2_val.pop();
-            s2_min.pop();
-            cout << last_element << endl;
-        }
-        else if (n == 3)
-        {
-            long long idx;
-            cin >> idx;
-
-            vector<long long> temp;
-
-            while (!s2_val.empty())
-            {
-                temp.push_back(s2_val.top());
-                s2_val.pop();
-            }
-
-            reverse(temp.begin(), temp.end());
-
-            while (!s1_val.empty())
-            {
-                temp.push_back(s1_val.top());
-                s1_val.pop();
-            }
-
-            cout << temp[idx - 1] << endl;
-
-            for (long long j = temp.size() - 1; j >= 0; --j)
-            {
-                s1_val.push(temp[j]);
-
-                s1_min.push(s1_min.empty() ? temp[j] : min(temp[j], s1_min.top()));
-            }
-        }
-        else if (n == 4)
-        {
-            long long min_s1, min_s2;
-
-            if (s1_min.empty())
-            {
-                min_s1 = 10^18;
-            }
-            else
-            {
-                min_s1 = s1_min.top();
-            }
-
-            if (s2_min.empty())
-            {
-                min_s2 = 10^18;
-            }
-            else
-            {
-                min_s2 = s2_min.top();
-            }
-
-            cout << min(min_s1, min_s2) << endl;
-        }
+      if (i < s2.size()) {
+        cout << s2[s2.size() - 1 - i] << endl;
+      } else {
+        cout << s1[i - s2.size()] << endl;
+      }
+    } else {
+      if (min_s2.empty()) {
+        cout << min_s1.back() << endl;
+      } else if (min_s1.empty()) {
+        cout << min_s2.back() << endl;
+      } else {
+        cout << min(min_s1.back(), min_s2.back()) << endl;
+      }
     }
-
-    return 0;
+  }
 }
